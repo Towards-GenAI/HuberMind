1f8c60ee8f4d9457ea1d3806c47fb6967b0ba973
tushar27 <tushar.27041994@gmail.com>
2024-05-28 17:30:21 +0530

    HuberMind Structure

---
+++ src/prompts/prompts.py
@@ -0,0 +1,3 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##HuberMind[Towards-GenAI] (https://github.com/Towards-GenAI)
+##################################################################################################
